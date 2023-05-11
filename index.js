2d9a10e8bc11bdb1ed947397a9b1810488806ec9
Dmytro Shpringer <shpringerd@gmail.com>
2023-05-11 18:20:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ addButtonEl.addEventListener("click", function() {
     if (inputFieldEl.value) {
         let inputValue = inputFieldEl.value
         
-        kittyImg.src = "assets/cat.png"
+        kittyImg.src = "https://i.pinimg.com/736x/aa/c8/29/aac829889e740dbeece2cf04649f2a46.jpg"
     
         push(shoppingListInDB, inputValue)
     
